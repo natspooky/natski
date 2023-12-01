90e0b7df1fb8a325d76465175e7c0a329ccf7b65
natski <natspooky@gmail.com>
2023-12-01 12:11:52 +0000

    N 2.0

---
+++ advent.js
@@ -0,0 +1,53 @@
+
+
+
+
+
+
+fetch("test.txt")
+  .then((res) => res.text())
+  .then((text) => {
+    let array = text.split('\n')
+    calculat(array)
+   })
+  .catch((e) => console.error(e));
+
+
+
+function calculat(e){
+    let strLen =  e.length,
+    finalVals = [],
+    total = 0;
+    for(let i = 0; i < strLen; i++) {
+
+        let r = 0,
+        valueArray = []
+        for(let x = 0; x < 2; x++){
+
+            r += 1
+
+            valueArray.push(checkbf(e, r, i))
+
+        }
+
+        finalVals.push((valueArray[0].toString()) + (valueArray[1].toString()))
+
+    }
+
+    finalVals.forEach((val) => {
+        total += Number(val)
+    })
+
+    console.log(total)
+}
+
+function checkbf(e, r, i){
+    for(let y = 0; y < e[i].length; y++) {
+
+        console.log(((r % 2 == 0) ? 0 : e[i].length), ((r % 2 == 0) ? 1 : -1))
+        if(!isNaN(e[i][ ((r % 2 == 0) ? 0 : e[i].length) + y * ((r % 2 == 0) ? 1 : -1)])) {
+            return e[i][ ((r % 2 == 0) ? 0 : e[i].length) + y * ((r % 2 == 0) ? 1 : -1)]
+        }
+
+    }
+}
