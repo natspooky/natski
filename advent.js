a19b0d41104a1fe5fc48a96d8d690e2f4e1fcbf2
natski <natspooky@gmail.com>
2023-12-01 18:56:15 +0000

    N 2.0

--- advent.js
+++ advent.js
@@ -53,7 +53,7 @@ function checkbf(e, r, i) {
 
 
 function getSTRnum(e, r, i, numList){
-    console.log(e[i+ 1])
+    console.log(e[i+ 1], i + 1)
     for(let y = 0; y < e[i].length - 5; y++) {
         let miniVal = e[i].slice(((r % 2 == 0) ? e[i].length - 6 : y), ((r % 2 == 0) ? (e[i].length - 1 - y) : y + 5));
         for(let v = 0; v < numList.length; v++) {
