cae41bec1c3c5cdb6bcc5fa9f3e910f6e629ccc6
natski <59707870+natspooky@users.noreply.github.com>
2024-12-05 23:27:51 +0000

    e

--- ENCORE/dependencies/ENCORE_SEC.mjs
+++ ENCORE/dependencies/ENCORE_SEC.mjs
@@ -4,32 +4,29 @@
  * MIT License
  */
 
-export function createElement(
-	elementType,
-	elementClass,
-	elementEvents,
-	elementAttributes,
-	innerHTML,
-	childElement,
-) {
-	let element = document.createElement(elementType);
+export function jsonElementify(elementData) {
+	let element = document.createElement(elementData.type);
 
-	if (elementClass) {
-		element.className = elementClass;
+	if (elementData.innerHTML) {
+		element.innerHTML = elementData.innerHTML;
 	}
 
-	if (innerHTML) {
-		element.innerHTML = innerHTML;
+	if (elementData.classes) {
+		for (const value of elementData.classes) {
+			element.classList.add(value);
+		}
 	}
 
-	if (elementAttributes) {
-		for (const [attribute, value] of Object.entries(elementAttributes)) {
+	if (elementData.attributes) {
+		for (const [attribute, value] of Object.entries(
+			elementData.attributes,
+		)) {
 			element.setAttribute(attribute, value);
 		}
 	}
 
-	if (elementEvents) {
-		for (const [eventType, event] of Object.entries(elementEvents)) {
+	if (elementData.events) {
+		for (const [eventType, event] of Object.entries(elementData.events)) {
 			element.addEventListener(
 				eventType,
 				event.var ? () => event.func(event.var) : () => event.func(),
@@ -37,30 +34,10 @@ export function createElement(
 		}
 	}
 
-	if (childElement) {
-		element.appendChild(childElement);
-	}
-
-	return element;
-}
-
-export function editElement(
-	element,
-	elementClass,
-	elementAttributes,
-	innerText,
-) {
-	if (elementClass) {
-		element.className = elementClass;
-	}
-
-	if (innerText) {
-		element.innerHTML = innerHTML;
-	}
-
-	if (elementAttributes) {
-		for (const [attribute, value] of Object.entries(elementAttributes)) {
-			element.setAttribute(attribute, value);
+	if (elementData.children) {
+		for (const child of elementData.children) {
+			console.log(child);
+			element.appendChild(jsonElementify(child));
 		}
 	}
 
@@ -71,4 +48,32 @@ export function appendChildren(element, children) {
 	for (const child of children) {
 		element.appendChild(child);
 	}
+}
+
+export function elementJsonify(element) {
+	let json = {};
+
+	json.type = element.tagName;
+
+	if (element.attributes) {
+		json.attributes = {};
+		for (const attribute of element.attributes) {
+			json.attributes[attribute.nodeName] = attribute.nodeValue;
+		}
+	}
+
+	if (element.className) {
+		json.classes = [...element.className.split(' ')];
+	}
+
+	if (element.children.length > 0) {
+		json.children = [];
+		for (const child of element.children) {
+			json.children.push(elementJsonify(child));
+		}
+	} else {
+		json.innerHTML = element.innerHTML;
+	}
+
+	return json;
 }
