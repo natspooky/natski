ecffdc516b309b43a8fdc30324f8036a94fb08e7
natski <59707870+natspooky@users.noreply.github.com>
2024-12-18 04:58:54 +0000

    N3.0

--- ENCORE/ENCORE_GIS.js
+++ ENCORE/ENCORE_GIS.js
@@ -4,7 +4,8 @@
  * MIT License
  */
 
-import * as GIS_data from 'https://natski.netlify.app/ENCORE/dependencies/ENCORE_GIS_DATASET.mjs';
+import { values } from 'https://natski.netlify.app/ENCORE/dependencies/ENCORE_GIS_DATASET.mjs';
+import * as DP from 'https://natski.netlify.app/ENCORE/dependencies/ENCORE_DP.mjs';
 
 var GISobj;
 
@@ -23,14 +24,35 @@ class GIS {
 		for (const icon of this.icons) {
 			if (
 				icon.hasAttribute('name') &&
-				GIS_data.values.includes(icon.getAttribute('name'))
+				values.includes(icon.getAttribute('name'))
 			) {
 				icon.style.mask = `url(https://natski.netlify.app/ENCORE/dependencies/GIS_icons/${icon.getAttribute(
 					'name',
 				)}.svg) no-repeat center`;
+			} else if (icon.hasAttribute('src')) {
+				let buffer = new Image();
+				buffer.onload = function () {
+					if (DP.getExtention(icon.getAttribute('src')) === '.svg') {
+						icon.style.mask = `url(${icon.getAttribute(
+							'src',
+						)}) no-repeat center`;
+					} else {
+						icon.style.backgroundImage = `url(${icon.getAttribute(
+							'src',
+						)}) no-repeat center`;
+					}
+					icon.style.mask = `url(${icon.getAttribute(
+						'src',
+					)}) no-repeat center`;
+				};
+				buffer.onerror = function () {
+					icon.style.mask = `url(https://natski.netlify.app/ENCORE/dependencies/GIS_icons/alert.svg) no-repeat center`;
+				};
+				buffer.src = icon.getAttribute('src');
 			} else {
 				icon.style.mask = `url(https://natski.netlify.app/ENCORE/dependencies/GIS_icons/alert.svg) no-repeat center`;
 			}
+			icon.style.visibility = 'visible';
 		}
 	}
 
