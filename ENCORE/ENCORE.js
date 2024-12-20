9b9ef134aab6dbefa522a218c8f2a74d90208f1f
natski <59707870+natspooky@users.noreply.github.com>
2024-12-20 05:27:57 +0000

    N3.0

--- ENCORE/ENCORE.js
+++ ENCORE/ENCORE.js
@@ -4,9 +4,9 @@
  * MIT License
  */
 
-export { SSCU } from 'https://natski.netlify.app/ENCORE/ENCORE_SSC_ULT.js';
-export { SSC } from 'https://natski.netlify.app/ENCORE/ENCORE_SSC.js';
-export { GIS } from 'https://natski.netlify.app/ENCORE/ENCORE_GIS.js';
-export { PAS } from 'https://natski.netlify.app/ENCORE/dependencies/ENCORE_PAS.mjs';
+export { default as SSCU } from 'https://natski.netlify.app/ENCORE/ENCORE_SSC_ULT.js';
+export { default as SSC } from 'https://natski.netlify.app/ENCORE/ENCORE_SSC.js';
+export { default as GIS } from 'https://natski.netlify.app/ENCORE/ENCORE_GIS.js';
+export { default as PAS } from 'https://natski.netlify.app/ENCORE/dependencies/ENCORE_PAS.mjs';
 export * as SEC from 'https://natski.netlify.app/ENCORE/dependencies/ENCORE_SEC.mjs';
 export * as DP from 'https://natski.netlify.app/ENCORE/dependencies/ENCORE_DP.mjs';
