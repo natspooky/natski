577256eeb53eaba23da10f9cca0d88fd1dafc4cd
natski <59707870+natspooky@users.noreply.github.com>
2025-05-27 17:27:42 +0100

    fix

--- api/dependencies/file-utils/fu.js
+++ api/dependencies/file-utils/fu.js
@@ -141,4 +141,5 @@ export function page() {}
 export function URLHash(URL) {}
 export function URLParameters(URL) {}
 */
-http://localhost:8080/index.html#api/encore/icon-system#burhger?embed=false
+/*
+http://localhost:8080/index.html#api/encore/icon-system#burhger?embed=false*/
